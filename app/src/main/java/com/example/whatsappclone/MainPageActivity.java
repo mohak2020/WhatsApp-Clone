b7e43afd9825fd657978bce7a5f1f7c672f24f0c
Mohammed Al-Khalifah <mohak2009@hotmail.com>
2019-07-07 00:43:17 +0300

    first commit

---
+++ app/src/main/java/com/example/whatsappclone/MainPageActivity.java
@@ -0,0 +1,39 @@
+package com.example.whatsappclone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.sign_put) {
+
+            FirebaseAuth.getInstance().signOut();
+
+
+        }
+        return true;
+    }
+}
